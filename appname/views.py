24d22903bac33939458cd7aac3b71d83c7588c6f
kavleen <kavleensaluja22@gmail.com>
2024-11-05 12:30:13 +0530

    index added

--- appname/views.py
+++ appname/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render, HttpResponse , HttpResponseRedirect
 # Create your views here.
 
 def home(request):
-    return render(request,'home.html')
+    return render(request,'index.html')
 def about(request):
     return render(request,'about.html')
 
